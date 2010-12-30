ab6759e86e30071fe1a0fdbd4dbd3a5a63a85a5d
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2010-12-30 00:22:00 -0500

    setting up folders and submodules

---
+++ config/sfDoctrineFacebookPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfDoctrineFacebookPlugin configuration.
+ * 
+ * @package     sfDoctrineFacebookPlugin
+ * @subpackage  config
+ * @author      Joshua Estes
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfDoctrineFacebookPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
